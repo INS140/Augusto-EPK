c4d3ae3f13bf02b3d2b80e309aa8f57b85c5dc08
INS140 <IsaacNSaettel@gmail.com>
2024-02-12 22:52:12 -0800

    videos

--- index.js
+++ index.js
@@ -40,7 +40,16 @@ const photosPage = `
 `
 
 const videosPage = `
-  
+  <div id="video">
+    <video width="200" height="200" controls><source src="./videos/Be_Mine.mp4" type="video/mp4" /></video>
+  </div>
+  <div class="video-select">
+    <button><img src="./images/Be_Mine_Cover_Img.jpg" alt="Be Mine" /></button>
+    <button><img src="./images/Spencer_Couture_Cover_Img.jpg" alt="Jam Session at Spencer Couture 1" /></button>
+    <button><img src="./images/Hey_by_If_Anything_Cover_Img.jpg" alt="Hey by If Anything" /></button>
+    <button><img src="./images/Fun_On_Stage_Cover_Img.jpg" alt="Jam Session at Spencer Couture 2" /></button>
+    <button><img src="./images/Sublime_Cover_Santeria_Cover_Img.jpg" alt="Sublime Cover Santeria" /></button>
+  </div>
 `
 
 // Initial Setup
@@ -63,4 +72,15 @@ photos.addEventListener("click", () => {
 videos.addEventListener("click", () => {
   outlet.innerHTML = videosPage
   outlet.classList = 'outlet videos'
+
+  const video = document.getElementById("video")
+  const buttons = document.querySelectorAll(".videos button")
+
+  const videoArray = ["Be_Mine.mp4", "Spencer_Couture.MP4", "Hey_by_If_Anything.mp4", "Fun_On_Stage.MP4", "Sublime_Cover_Santeria.MP4"]
+  
+  buttons.forEach((b, i) => {
+    b.addEventListener("click", () => {
+      video.innerHTML = `<video width="200" height="200" controls><source src="./videos/${videoArray[i]}" type="video/mp4" /></video>`
+    })
+  })
 })
